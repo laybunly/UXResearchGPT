c1b410cf15e1fad82a00175ab0d60404d8e2c224
laybunly <52610711+laybunly@users.noreply.github.com>
2023-06-13 13:59:41 +0200

    New Icons, New Welcome Page

--- Hello.py
+++ Hello.py
@@ -6,11 +6,14 @@ PAGES = {
     "Page 2: Journey Map": JourneyBuilder
 }
 
-st.title('Welcome to the Persona Creator and Journey Map Application!')
+st.title('Welcome to the UX Research GPT App!')
 st.write('''
     With this application, you can:
+    - Create Interview Questions: Maximize your user interview sessions by providing relevant information and receiving tailored interview questions.
     - Create a Persona: Enter information and receive a detailed description of the persona.
     - Create a Journey Map: Based on a created persona, you can generate a journey map.
+
+    To get started, simply select an app from the menu on the left side.
 ''')
 
 
