784d86a6970a2e2c065c4236b6de043ca498ec14
laybunly <52610711+laybunly@users.noreply.github.com>
2023-06-13 14:08:39 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,9 +9,14 @@ PAGES = {
 st.title('Welcome to the UX Research GPT App!')
 st.write('''
     With this application, you can:
-    - [Create Interview Questions 🗣️](https://uxresearchgpt.streamlit.app/InterviewQuestionsCreator): Maximize your user interview sessions by providing relevant information and receiving tailored interview questions.
-    - [Create a Persona 👤]:(https://uxresearchgpt.streamlit.app/PersonaCreator) Enter information and receive a detailed description of the persona.
-    - [Create a Journey Map 🛣️](https://uxresearchgpt.streamlit.app/JourneyBuilder): Based on a created persona, you can generate a journey map.
+    - [Create Interview Questions 🗣️](https://uxresearchgpt.streamlit.app/InterviewQuestionsCreator): 
+    Maximize your user interview sessions by providing relevant information and receiving tailored interview questions.
+
+    - [Create a Persona 👤](https://uxresearchgpt.streamlit.app/PersonaCreator): 
+    Enter information and receive a detailed description of the persona.
+    
+    - [Create a Journey Map 🛣️](https://uxresearchgpt.streamlit.app/JourneyBuilder): 
+    Based on a created persona, you can generate a journey map.
 
     To get started, simply select an app from the menu on the left side or click on the links in the description above.
 
