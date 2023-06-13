1296d9b1fc236e2a5d906e80ad535d4d868a0b8a
laybunly <52610711+laybunly@users.noreply.github.com>
2023-06-13 14:14:09 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,27 +1,26 @@
 import streamlit as st
-from pages import InterviewQuestions, PersonaCreator, JourneyBuilder
-
-PAGES = {
-    "Create Interview Questions": InterviewQuestions,
-    "Create a Persona": PersonaCreator,
-    "Create a Journey Map": JourneyBuilder
-}
 
 st.title('Welcome to the UX Research GPT App!')
 
-col1, col2, col3 = st.beta_columns(3)
+# Define the apps and their descriptions
+apps = {
+    'Create Interview Questions': 'Maximize your user interview sessions by providing relevant information and receiving tailored interview questions.',
+    'Create a Persona': 'Enter information and receive a detailed description of the persona.',
+    'Create a Journey Map': 'Based on a created persona, you can generate a journey map.'
+}
 
+# Create three columns
+col1, col2, col3 = st.columns(3)
+
+# Render each app in a separate column
 with col1:
-    st.header("Create Interview Questions")
-    st.write("Maximize your user interview sessions by providing relevant information and receiving tailored interview questions.")
-    st.write("Description of the app goes here.")
+    st.header('Create Interview Questions')
+    st.write(apps['Create Interview Questions'])
 
 with col2:
-    st.header("Create a Persona")
-    st.write("Enter information and receive a detailed description of the persona.")
-    st.write("Description of the app goes here.")
+    st.header('Create a Persona')
+    st.write(apps['Create a Persona'])
 
 with col3:
-    st.header("Create a Journey Map")
-    st.write("Based on a created persona, you can generate a journey map.")
-    st.write("Description of the app goes here.")
+    st.header('Create a Journey Map')
+    st.write(apps['Create a Journey Map'])
