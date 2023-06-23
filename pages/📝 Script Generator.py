bb5e83953dc4bfd1254ace67b39905403494109b
laybunly <52610711+laybunly@users.noreply.github.com>
2023-06-23 16:15:20 +0200

    Update 📝 Script Generator.py

--- pages/📝 Script Generator.py
+++ pages/📝 Script Generator.py
@@ -12,12 +12,14 @@ os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 # App framework
 st.title('📝  Script Generator')
 
-# Include text in the sidebar
 with st.sidebar:
-    st.write("This script generator is a tool that helps you create training video scripts. All you need to do is input the key points or topics that you want to include in your training video. These can be entered in a simple bullet point format. Once you've input the topics, the tool takes over. \n It uses the power of artificial intelligence to transform these bullet points into a full script that can be used for your training video. It writes out everything that the narrator would need to say to cover all the points you've entered. \nSo, you can think of this as an AI assistant that helps you write scripts for your training videos. It's designed to make the script writing process quicker and easier, allowing you to focus on the other aspects of creating your training content.")
+    st.markdown("""
+    This script generator is a tool that helps you create training video scripts. All you need to do is input the key points or topics that you want to include in your training video. These can be entered in a simple bullet point format. Once you've input the topics, the tool takes over.<br><br>
+    It uses the power of artificial intelligence to transform these bullet points into a full script that can be used for your training video. It writes out everything that the narrator would need to say to cover all the points you've entered.<br><br>
+    So, you can think of this as an AI assistant that helps you write scripts for your training videos. It's designed to make the script writing process quicker and easier, allowing you to focus on the other aspects of creating your training content.
+    """, unsafe_allow_html=True)
 
-
-# Create the input fields with placeholders
+# Create the input fields with placeholders 
 persona_description = st.text_area('What is the content of your video?', placeholder="Step 1: Global Opportunity page Layout, Step 1.1: Account Information: = is linked to the Legal Entity account including 1.1) Territory", height=200)
 
 # Prompt templates
